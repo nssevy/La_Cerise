43a2f167ce0174fd1bb01d3d171c51a65ff77d49
sevy <sevyns.pro@gmail.com>
2026-04-04 21:46:21 +0200

    feat: ajouter la page lexique avec intégration base de données et template Twig

---
+++ public/lexique.php
@@ -0,0 +1,21 @@
+<?php
+require_once dirname(__DIR__) . '/config/db.php';
+require_once dirname(__DIR__) . '/config/twig.php';
+
+$termes = $pdo->query("
+    SELECT terme, categorie, definition
+    FROM lexique
+    ORDER BY terme ASC
+")->fetchAll(PDO::FETCH_ASSOC);
+
+// Grouper les termes par première lettre
+$termesParLettre = [];
+foreach ($termes as $terme) {
+    $lettre = mb_strtoupper(mb_substr($terme['terme'], 0, 1));
+    $termesParLettre[$lettre][] = $terme;
+}
+ksort($termesParLettre);
+
+echo $twig->render('lexique.html.twig', [
+    'termesParLettre' => $termesParLettre,
+]);
