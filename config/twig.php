44b3cf0f0350b7c0b5386d4ce4dccd79e4df9bec
sevy <sevyns.pro@gmail.com>
2026-04-04 22:10:19 +0200

    feat: préparer le déploiement en production avec gestion des environnements

--- config/twig.php
+++ config/twig.php
@@ -5,9 +5,13 @@ require_once dirname(__DIR__) . '/includes/helpers.php';
 $loader = new \Twig\Loader\FilesystemLoader(dirname(__DIR__) . '/templates');
 
 $twig = new \Twig\Environment($loader, [
-    'cache' => false,
-    'debug' => true,
+    'cache' => $_ENV['APP_ENV'] === 'production'
+        ? dirname(__DIR__) . '/cache/twig'
+        : false,
+    'debug' => $_ENV['APP_ENV'] !== 'production',
 ]);
+
+$twig->addGlobal('base', $_ENV['BASE_URL'] ?? '');
 
 // La fonction helper
 $twig->addFunction(new \Twig\TwigFunction('formatDateFr', function ($date) {
