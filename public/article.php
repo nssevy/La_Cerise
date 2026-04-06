1d7cf57a6d429dcdb6b2fb4db1ccac7e4f6b29b3
sevy <sevyns.pro@gmail.com>
2026-04-07 01:07:05 +0200

    feat: ajouter table des matières, boutons retour et partager sur la page article

--- public/article.php
+++ public/article.php
@@ -44,8 +44,39 @@ $stmtSuggeres = $pdo->prepare('
 $stmtSuggeres->execute([':id' => $article['id']]);
 $suggeres = $stmtSuggeres->fetchAll(PDO::FETCH_ASSOC);
 
+// Table des matières
+$toc = [];
+if ($article['contenu']) {
+    $dom = new DOMDocument();
+    libxml_use_internal_errors(true);
+    $dom->loadHTML('<?xml encoding="utf-8"?><div id="wrapper">' . $article['contenu'] . '</div>');
+    libxml_clear_errors();
+
+    $h2s = $dom->getElementsByTagName('h2');
+    $h2sArray = iterator_to_array($h2s);
+
+    foreach ($h2sArray as $h2) {
+        if (!($h2 instanceof DOMElement))
+            continue;
+
+        $texte = $h2->textContent;
+        $ancre = strtolower(preg_replace('/[^a-z0-9]+/i', '-', $texte));
+        $toc[] = ['texte' => $texte, 'ancre' => $ancre];
+        $h2->setAttribute('id', $ancre);
+    }
+
+    // Récupérer uniquement le contenu du wrapper
+    $wrapper = $dom->getElementById('wrapper');
+    $contenuAvecAncres = '';
+    foreach ($wrapper->childNodes as $node) {
+        $contenuAvecAncres .= $dom->saveHTML($node);
+    }
+    $article['contenu'] = $contenuAvecAncres;
+}
+
 echo $twig->render('article.html.twig', [
     'article' => $article,
     'lecture' => $lecture,
     'suggeres' => $suggeres,
+    'toc' => $toc,
 ]);
