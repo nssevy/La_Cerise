929ed9cfcc5277ef582ba7f0e1389e2e9e5bd793
Yves <sevyns.pro@gmail.com>
2026-06-08 14:25:54 +0200

    feat: liens rubriques du footer vers allArticles avec filtre pré-sélectionné

--- config/twig.php
+++ config/twig.php
@@ -13,6 +13,9 @@ $twig->addGlobal('base', $_ENV['BASE_URL'] ?? '');
 $twig->addGlobal('user_nom', $_SESSION['user_nom'] ?? '');
 $twig->addGlobal('csrf_token', csrf_generate());
 
+$rubriquesStmt = $pdo->query('SELECT id, nom FROM rubriques ORDER BY nom');
+$twig->addGlobal('rubriquesNav', $rubriquesStmt->fetchAll());
+
 $twig->addFunction(new \Twig\TwigFunction('formatDateFr', function ($date) {
     return formatDateFr($date);
 }));
