c8292cdac9b89d539cc53b7172c240d1d7f44f45
Yves <sevyns.pro@gmail.com>
2026-06-09 16:33:05 +0200

    Fix : Sticky tool bar dans TinyMce

--- assets/js/tinymce-init.js
+++ assets/js/tinymce-init.js
@@ -1,12 +1,13 @@
+const navOffset = document.querySelector('#admin-nav')?.offsetHeight ?? 0;
+
 tinymce.init({
   selector: "#contenu",
   language: "fr_FR",
-  plugins: "lists link image code",
-  toolbar:
-    "undo redo | bold italic | h2 | bullist numlist | blockquote | link image | code",
+  toolbar: "undo redo | bold italic | h2 | bullist numlist | blockquote | link image | code",
   menubar: false,
-  selector: "#contenu",
-  plugins: "autoresize bold italic lists link image code",
+  plugins: "autoresize lists link image code",
   autoresize_bottom_margin: 0,
   min_height: 300,
+  toolbar_sticky: true,
+  toolbar_sticky_offset: navOffset,
 });
